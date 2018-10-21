40c5334c1c11a054252dba8663116d9029688aa4
Maciek <maciek.d@me.com>
2018-10-21 00:42:26 -0700

    Added Models

---
+++ SimpleCrud/Models/AddUserModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SimpleCrud.Models
+{
+    public class AddUserModel
+    {
+        [Required(ErrorMessage ="First name must be entered!")]
+        [MinLength(3, ErrorMessage = "Last name can't be shorter than 3 characters!")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name must be entered!")]
+        [MinLength(3, ErrorMessage ="Last name can't be shorter than 3 characters!")]
+        public string LastName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ SimpleCrud/Models/EditUserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleCrud.Models
+{
+    public class EditUserModel
+    {
+        public long Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
